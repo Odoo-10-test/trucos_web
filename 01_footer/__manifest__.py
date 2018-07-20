1d65f4d6736457f00eed6d1a4311be04c2fb509d
marlon <falconsoft.3d@gmail.com>
2018-07-19 22:34:31 -0400

    Actualizando

---
+++ 01_footer/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2018 Marlon Falcón Hernandez
+#    (<http://www.falconsolutions.cl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Footer Website 01 MFHW',
+    'version': '10.0.0.1.0',
+    'author': "Falcon Solutions SpA",
+    'maintainer': "Falcon Solutions SpA",
+    'website': 'http://www.falconsolutions.cl',
+    'license': 'AGPL-3',
+    'category': 'Whatsapp',
+    'summary': 'Web Login',
+    'description': """
+whatsapp
+=====================================================
+1-. whatsapp.\n
+""",
+    'depends': ['website', 'website_blog', 'sale'],
+    'data': [
+        'templates/footer.xml'
+    ],
+    'images': ['static/description/banner.jpg'],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+
+}
